8afe39a4653590d8b8119f228153efaa8187745d
akat32 <baduk32127@gmail.com>
2018-08-04 17:55:04 +0900

    add token login

--- routes/auth/auto.js
+++ routes/auth/auto.js
@@ -1,3 +1,8 @@
 module.exports = (router, Users)=>{
+  router.get('/:token', async (req,res)=>{ // D5qS3kxBAavQiaNrdZav1au
+    var result = await Users.findOne({token : req.params.token})
+    if(!result) return res.status(404).json({message : "User not found!"})
+    else return res.status(200).json(result)
+  })
   return router;
 }
